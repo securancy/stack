bcfc497f99339ae79097299b4f846919d3888957
hanssens <github@hanssens.com>
2015-04-29 10:32:16 +0200

    Initial commit of clean, empty solution workspace

---
+++ source/Securancy.Net.Common/Foo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Securancy.Net.Common
+{
+    public class Foo
+    {
+        public string Bar()
+        {
+            return "Foo";
+        }
+    }
+}
